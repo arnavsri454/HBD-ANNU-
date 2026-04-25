4da689bbe5193cc41ff57e0bf0a67dff78eab70a
arnavsri454 <arnavsri454@gmail.com>
2026-04-25 14:57:22 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||HAPPY|BIRTHDAY|ANUSHA|❤|#rectangle|May you Bless|YOU|,|Have strength| and carry yourself| through hardships ');
+      S.UI.simulate('|#countdown 3||HAPPY|BIRTHDAY|ANUSHA|❤|#rectangle|May you Bless|YOU|,|Have strength| and carry| yourself| through| hardships ');
     }
 
     S.Drawing.loop(function () {
