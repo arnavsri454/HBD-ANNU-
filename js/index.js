44335e77a98c9548ac75940ddd988a3c1935c27f
arnavsri454 <arnavsri454@gmail.com>
2026-08-11 22:19:55 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||HAPPY|BIRTHDAY|Anvesha |❤|#rectangle|May Gog Bless|YOU|,|Have strength|Courage |Stay Happy | ');
+      S.UI.simulate('|#countdown 3||HAPPY|BIRTHDAY|Anvesha |❤|#rectangle|May God| Bless|YOU|,|Have| strength|Courage |Stay |Happy | ');
     }
 
     S.Drawing.loop(function () {
